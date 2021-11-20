3b885dcdd8e95be3cf45223f7d5f92bac65541ea
RichardL64 <92684598+RichardL64@users.noreply.github.com>
2021-11-20 18:47:59 +0000

    Tweak pull/release detection

--- PinballY/Spin.js
+++ PinballY/Spin.js
@@ -7,7 +7,7 @@
 //	R.Lincoln		November 2021		Creation
 //
 
-const mySlow = 0.5;					// speed rate threshold fast vs. slow
+const mySlow = 0.3;					// speed rate threshold fast vs. slow
 const myDeccel = 0.9;					// multiplier to slow the wheel spin speed
 const myFriction = 0.5;					// subtracted from wheel spin
 const myLaunchDelay = 1000;				// milliseconds to wait between plunger release and game launch
