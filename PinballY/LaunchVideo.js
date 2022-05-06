1e2ef7c4a53c4c48402df3d13020dbefca6ebc91
Richard Lincoln <92684598+RichardL64@users.noreply.github.com>
2022-05-06 16:27:14 +0100

    Support game specific videos
    Tries the game name first, if that doesn't exist tries launch.mp4

--- PinballY/LaunchVideo.js
+++ PinballY/LaunchVideo.js
@@ -3,17 +3,30 @@
 //	https://github.com/RichardL64
 //
 //	Play video during game launch
-//	Plays	../PinballY/Media/Videos/Launch.mp4
+//
+//	Plays			../PinballY/Media/Videos/<Game Name (Manufacturer Year>.mp4
+//	If it doesnt exist	../PinballY/Media/Videos/Launch.mp4
 //
 //	R.Lincoln	April 2022
 //
 
 
 //	On game launch
+//	Try the game display name e.g. "Terminator 2 Judgement Day (Williams 1991).mp4"
+//	If it doesnt exist try "launch.mp4"
 //
 function myOverlayShow(ev) {
-	mainWindow.launchOverlay.bg.clear(0xff000000);
-	let video = gameList.resolveMedia("Videos","Launch.mp4" );
+	var name, video
+
+	name = mainWindow.getUIMode().game.displayName;
+	logfile.log("[Launch Video] Trying: %s", name);
+	video = gameList.resolveMedia("Videos", name, "Video" );
+
+	if(video === undefined) {
+		logfile.log("[Launch Video] Trying: Launch");
+		video = gameList.resolveMedia("Videos","Launch", "Video" );
+	}
+
 	mainWindow.launchOverlay.bg.loadVideo(video);
 	ev.preventDefault();
 }
