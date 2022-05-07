71cde41412812472544047d1b3d49c9cccf6385e
Richard Lincoln <92684598+RichardL64@users.noreply.github.com>
2022-05-07 09:01:36 +0100

    Debug loading message

--- PinballY/Startup.js
+++ PinballY/Startup.js
@@ -16,7 +16,7 @@
 var game, games
 game = -1									// invalid index to avoid overriding messages
 games = gameList.getFilterInfo("Category.Startup").getGames();			// 0 based array of all games in startup category
-logfile.log("[Startup] %d games in startup category", games.length);
+logfile.log("[Startup] #%d games in startup category", games.length);
 
 //	If there was at least one game to choose from, start it
 //	But.. .after a delay to release the thread or PBY does wierd things when you exit the game
@@ -29,7 +29,6 @@ function startupGame() {
 	game = Math.floor(Math.random() * games.length);			// 0 based index of one of them at random
 	logfile.log("[Startup] Choose #%d: %s", game +1, games[game].displayName);
 	mainWindow.playGame(games[game])					// Start it!
-	game = -1;								// make sure we dont override messages later
 }
 
 //	Overlay message
@@ -37,6 +36,7 @@ function startupGame() {
 function myOverlayMessage(ev) {
 	if(game > -1 && ev.message == "Loading...") {
 		ev.message = "Loading startup game..."
+		game = -1;							// make sure we dont override messages later
 	}
 }
 
